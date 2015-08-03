640f556ceb4728d36bf1cf0440b8d4ab1a46043d
Robin Scott <robinscott@btinternet.com>
2015-08-03 16:25:10 +0100

    Added Yeoman Angular.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Shopping basket front-end project
+# shopping-basket
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.12.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shoppingBasketApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shoppingBasketApp
+ */
+angular.module('shoppingBasketApp')
+  .controller('MainCtrl', function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Robin'
+    ];
+  });
