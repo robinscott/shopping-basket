6db96d1d5e49559abb688c34737a1b7d3c89c497
Robin Scott <robinscott@btinternet.com>
2015-08-12 10:24:59 +0100

    RS - product details now working.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,19 +16,24 @@ angular.module('shoppingBasketApp')
             section3Collapsed: true
         };
 
-        $scope.product = {
+        $scope.productDetails = {
             chosenProduct: '',
             products: [],
-            userQuantity: 0
-        };
-
-        $scope.cost = function(price) {
-            return price * product.userQuantity;
+            user1Quantity: 0,
+            calculateUnitcost: function(unitcost, discount) {
+                discount == 0 ? discount = 1 : discount;
+                return unitcost * discount;
+            },
+            calculateTotal: function(unitcost, discount) {
+                discount == 0 ? discount = 100 : discount;
+                discount = discount / 100;
+                return (unitcost * discount) * this.user1Quantity ;
+            }
         };
 
         $http.get('scripts/prices.json').
             then(function (response) {
-                $scope.product.products = response.data;
+                $scope.productDetails.products = response.data;
             }, function (response) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
