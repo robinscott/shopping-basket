1f08877a7e89c3e962f1ed0a0d16b593136b5add
Robin Scott <robinscott@btinternet.com>
2015-09-10 20:26:53 +0100

    RS - removed explicit parameters from functions.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,23 +26,23 @@ angular.module('shoppingBasketApp')
         self.userSubTotalUnitcost = 0;
 
 
-        self.calculateUnitcost = function (quantity) {
-            if (self.selectedProduct) {
-                return self.selectedProduct.unitcost * quantity;
+        self.calculateUnitcost = function (selectedProduct, quantity) {
+            if (selectedProduct) {
+                return selectedProduct.unitcost * quantity;
             }
         };
 
-        self.calculateCostPerUser = function (discount) {
-            if (self.selectedProduct) {
+        self.calculateCostPerUser = function (selectedProduct, discount) {
+            if (selectedProduct) {
                 discount = (100 - discount) / 100;
-                return self.selectedProduct.unitcost * discount;
+                return selectedProduct.unitcost * discount;
             }
         };
 
-        self.calculateSubTotal = function (discount, quantity, userQuantity) {
-            if (self.selectedProduct) {
+        self.calculateSubTotal = function (selectedProduct, discount, quantity, userQuantity) {
+            if (selectedProduct) {
                 discount = (100 - discount) / 100;
-                var discountedUnit = self.selectedProduct.unitcost * discount;
+                var discountedUnit = selectedProduct.unitcost * discount;
                 return (discountedUnit * quantity) * userQuantity;
             }
         };
@@ -51,18 +51,22 @@ angular.module('shoppingBasketApp')
             return (discount / 100) * subTotalUnitcost;
         };
 
+        self.calculateSupportYearSubtotal = function (chosenSupport, userLicense, userQuantity) {
+            return (chosenSupport * userLicense) * userQuantity;
+        };
+
         self.updateSelectedProduct = function () {
             self.selectedProduct.userLicensePacks = {
                 pack1: {
                     amount: 1,
-                    unitCost: self.calculateUnitcost(1),
+                    unitCost: self.calculateUnitcost(self.selectedProduct, 1),
                     discountPack: 0,
                     costPerUser: self.selectedProduct.unitcost,
                     updateQuantity: function () {
 
                         // Calculate basket subtotal
-                        this.userSubTotal = self.calculateSubTotal(0, this.userQuantity, 1);
-                        self.userSubTotalUnitcost = self.calculateUnitcost(1) * this.userQuantity;
+                        this.userSubTotal = self.calculateSubTotal(self.selectedProduct, 0, this.userQuantity, 1);
+                        self.userSubTotalUnitcost = self.calculateUnitcost(self.selectedProduct, 1) * this.userQuantity;
 
                         self.userDiscount = 0;
                         self.userQuantity = this.userQuantity;
@@ -72,14 +76,14 @@ angular.module('shoppingBasketApp')
                 },
                 pack5: {
                     amount: 5,
-                    unitCost: self.calculateUnitcost(5),
+                    unitCost: self.calculateUnitcost(self.selectedProduct, 5),
                     discountPack: self.selectedProduct.discount5pack,
-                    costPerUser: self.calculateCostPerUser(self.selectedProduct.discount5pack),
+                    costPerUser: self.calculateCostPerUser(self.selectedProduct, self.selectedProduct.discount5pack),
                     updateQuantity: function () {
 
                         // Calculate basket subtotal
-                        this.userSubTotal = self.calculateSubTotal(self.selectedProduct.discount5pack, this.userQuantity, 5);
-                        self.userSubTotalUnitcost = self.calculateUnitcost(5) * this.userQuantity;
+                        this.userSubTotal = self.calculateSubTotal(self.selectedProduct, self.selectedProduct.discount5pack, this.userQuantity, 5);
+                        self.userSubTotalUnitcost = self.calculateUnitcost(self.selectedProduct, 5) * this.userQuantity;
 
                         // Calculate summary discount amount
                         self.userDiscountAmount = self.calculateDiscountAmount(self.selectedProduct.discount5pack, self.userSubTotalUnitcost);
@@ -92,14 +96,14 @@ angular.module('shoppingBasketApp')
                 },
                 pack10: {
                     amount: 10,
-                    unitCost: self.calculateUnitcost(10),
+                    unitCost: self.calculateUnitcost(self.selectedProduct, 10),
                     discountPack: self.selectedProduct.discount10pack,
-                    costPerUser: self.calculateCostPerUser(self.selectedProduct.discount10pack),
+                    costPerUser: self.calculateCostPerUser(self.selectedProduct, self.selectedProduct.discount10pack),
                     updateQuantity: function () {
 
                         // Calculate basket subtotal
-                        this.userSubTotal = self.calculateSubTotal(self.selectedProduct.discount10pack, this.userQuantity, 10);
-                        self.userSubTotalUnitcost = self.calculateUnitcost(10) * this.userQuantity;
+                        this.userSubTotal = self.calculateSubTotal(self.selectedProduct, self.selectedProduct.discount10pack, this.userQuantity, 10);
+                        self.userSubTotalUnitcost = self.calculateUnitcost(self.selectedProduct, 10) * this.userQuantity;
 
                         // Calculate summary discount amount
                         self.userDiscountAmount = self.calculateDiscountAmount(self.selectedProduct.discount10pack, self.userSubTotalUnitcost);
@@ -116,7 +120,7 @@ angular.module('shoppingBasketApp')
                     year: 1,
                     amount: self.selectedProduct.support1year,
                     updateSupport: function () {
-                        self.supportYearSubtotal = (self.chosenSupport * self.userLicense) * self.userQuantity;
+                        self.supportYearSubtotal = self.calculateSupportYearSubtotal(self.chosenSupport, self.userLicense, self.userQuantity);
                         self.supportYear = 1;
                     }
                 },
@@ -124,7 +128,7 @@ angular.module('shoppingBasketApp')
                     year: 2,
                     amount: self.selectedProduct.support2year,
                     updateSupport: function () {
-                        self.supportYearSubtotal = (self.chosenSupport * self.userLicense) * self.userQuantity;
+                        self.supportYearSubtotal = self.calculateSupportYearSubtotal(self.chosenSupport, self.userLicense, self.userQuantity);
                         self.supportYear = 2;
                     }
                 },
@@ -132,7 +136,7 @@ angular.module('shoppingBasketApp')
                     year: 3,
                     amount: self.selectedProduct.support3year,
                     updateSupport: function () {
-                        self.supportYearSubtotal = (self.chosenSupport * self.userLicense) * self.userQuantity;
+                        self.supportYearSubtotal = self.calculateSupportYearSubtotal(self.chosenSupport, self.userLicense, self.userQuantity);
                         self.supportYear = 3;
                     }
                 }
@@ -142,10 +146,10 @@ angular.module('shoppingBasketApp')
         self.updateTotal = function () {
             var subtotal = self.userSubTotalUnitcost;
             if (self.userDiscountAmount) {
-                subtotal -= self.userDiscountAmount;
+                subtotal = subtotal - self.userDiscountAmount;
             }
             if (self.supportYearSubtotal) {
-                subtotal += self.supportYearSubtotal;
+                subtotal = subtotal + self.supportYearSubtotal;
             }
             return subtotal;
         };
